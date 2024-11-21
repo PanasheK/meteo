1ace07b1dacf02b8bd24b739c6636b3141ce8695
PanasheK <92722503+PanasheK@users.noreply.github.com>
2024-11-21 16:20:26 +0200

    updated weather search function

--- src/script.js
+++ src/script.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "7314ct9o08bb3ff968030b24667afbe3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query= ${city} &key=${apikey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement.value;
+
+  searchCity(searchInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+searchCity("New York");
